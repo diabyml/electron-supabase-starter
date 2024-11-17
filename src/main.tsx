4c83e24947b5be707019baf91fbcf87cf25e3374
diabyml <diaby223.ml@gmail.com>
2024-11-17 15:38:47 +0000

    deleted react-router-dom

--- src/main.tsx
+++ src/main.tsx
@@ -1,44 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import App from "./App";
 import "./index.css";
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
-import Root from "./routes/root";
-import ErrorPage from "./routes/error";
-import About from "./routes/about";
-import Test from "./routes/test";
-
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <Root />,
-    errorElement: <ErrorPage />,
-    children: [
-      {
-        index: true,
-        element: <div>index</div>,
-      },
-      {
-        path: "contacts",
-        element: <div>contacts</div>,
-      },
-      {
-        path: "archives",
-        element: <div>archives</div>,
-      },
-    ],
-  },
-  {
-    path: "/about",
-    element: <About />,
-  },
-  {
-    path: "/test/:id",
-    element: <Test />,
-  },
-]);
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
-    <RouterProvider router={router} />
+    <App />
   </React.StrictMode>
 );
